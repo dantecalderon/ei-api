79d5f16bbf42ff4e77616e10058cc28bf1affd4f
Joao Silva <joao.galiano.silva@gmail.com>
2018-02-09 12:45:36 +0000

    Remove bin folder

--- bin/app/decorators/request-object.decorator.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const common_1 = require("@nestjs/common");
-exports.RequestObjectDecorator = common_1.createRouteParamDecorator((data, req) => req[data]);
-//# sourceMappingURL=request-object.decorator.js.map
--- bin/app/providers/password.provider.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PASSWORD = { saltRounds: 5 };
-//# sourceMappingURL=password.provider.js.map
